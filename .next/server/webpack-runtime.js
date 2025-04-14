34a8d39c8021182e29e6aadea965f90993febcf1
Vignesh <vigneshg1001+github@gmail.com>
2025-04-14 12:23:56 +0530

    added sidebars on the account, support, view simulation page

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("d5b329bfde3d6d5f")
+/******/ 		__webpack_require__.h = () => ("e106f2b9e13fea60")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
